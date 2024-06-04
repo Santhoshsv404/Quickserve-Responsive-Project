30d482db93e315fe8f09ede5ca0271bc0005c672
Santhosh S V <142136987+Santhoshsv404@users.noreply.github.com>
2024-06-04 13:34:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+
+
+var menu_button = document.querySelector(".menu_button");
+var side_bar = document.querySelector(".side_bar");
+
+function opensidebar(){
+    
+    side_bar.style.display="block";
+
+}
+function closesidebar(){
+    side_bar.style.display="none"
+}
+
+
